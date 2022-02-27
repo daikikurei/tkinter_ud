2373a71ec53d0764462b61da3d04e9ddf64b21fc
daikikurei <ai32oa36kid14hcet@gmail.com>
2022-02-28 00:14:26 +0900

    update:   "greeting.py"
    ラジオボタンの作成を記述した。

--- section3_挨拶アプリ/greeting.py
+++ section3_挨拶アプリ/greeting.py
@@ -15,7 +15,7 @@ output_color = '#A9A9A9'
 input_frame = tkinter.Frame(root) #rootウィンドウにフレームを作る
 output_frame = tkinter.LabelFrame(root, bg=output_color) #bg:背景色 #https://ironodata.info/search/
 input_frame.pack(pady=10)
-output_frame.pack(fill='both', expand=True) #fill='both':横縦共に引き延ばされた状態になる #expand=True:縦方向にも引き延ばしたい場合
+output_frame.pack(fill='both', expand=True, padx=10, pady=(0, 10)) #fill='both':横縦共に引き延ばされた状態になる #expand=True:縦方向にも引き延ばしたい場合 #pady=(0, 10):input_frameにも下側に余白があるので、上側の余白は0にしておく。
 
 #ボタン画像の読み込み
 submit_img = tkinter.PhotoImage(file='./icon/submit.png')
@@ -25,8 +25,18 @@ submit_img = tkinter.PhotoImage(file='./icon/submit.png')
 name = tkinter.Entry(input_frame, width=30)
 name.insert(0, '名前を入力してください')
 submit_button = tkinter.Button(input_frame, image=submit_img)
-name.grid(row=0, column=0, padx=10, pady=10)
-submit_button.grid(row=0, column=1, padx=10, pady=10)
+name.grid(row=0, column=0, padx=10, pady=10, columnspan=3) #columnspan=3:3つのcolumnを1つと扱う
+submit_button.grid(row=0, column=3, padx=10, pady=10) #column=3:0,1,2,3の3に配置した。
+
+#ラジオボタンの作成
+radio_value = tkinter.StringVar() #文字列を入れるための変数を定義
+radio_value.set('morning') #最初に選択される値
+morning_button = tkinter.Radiobutton(input_frame, text='朝', variable=radio_value, value='morning')
+noon_button = tkinter.Radiobutton(input_frame, text='昼', variable=radio_value, value='noon')
+night_button = tkinter.Radiobutton(input_frame, text='夜', variable=radio_value, value='night')
+morning_button.grid(row=1, column=0) #エントリー&ボタンの下側にラジオボタンを作りたいので、row=1
+noon_button.grid(row=1, column=1) #mornignの横に並べたいのでcolumn=1
+night_button.grid(row=1, column=2) #noonの横に並べたいのでcolumn=1
 
 #ウィンドウのループ処理
 root.mainloop()
